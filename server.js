d0d22923e1efea6c418b1e2a4f0f24ec82e8345b
njabuloszulu <njabuloszulu@gmail.com>
2020-11-06 15:48:00 +0200

    changing the structure for deployment

--- server.js
+++ server.js
@@ -9,4 +9,11 @@ app.use(bodyParser.json());
 Routes(app);
 const PORT = 8000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => console.log(`app running on ${PORT}`));
